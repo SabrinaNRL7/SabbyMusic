84db84e8c23bd83e7889960eac1605c25ebb3802
MobileAppProgramming <s21810001@student.unklab.ac.id>
2021-05-08 23:23:22 +0800

    adding SignIn and SignUp link in the HomeScreen

--- src/components/molecules/NavBar/index.js
+++ src/components/molecules/NavBar/index.js
@@ -12,7 +12,7 @@ const NavBar = () => {
             <div className="navbar-nav">
             <Link className="nav-link" to="/" >Home</Link>             
             <Link className="nav-link" to="/dashboard" >Dashboard</Link>             
-            <Link className="nav-link" to="/about" >About</Link>             
+            <Link className="nav-link" to="/about" >About</Link>                          
             </div>
           </div>
         </div>
--- src/components/pages/HomeScreen/index.js
+++ src/components/pages/HomeScreen/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import NavBar from '../../molecules/NavBar';
 import { Container } from "react-bootstrap";
 import Loggo from "../../../assets/Logo/SabbyMusicLoggo.jpg";
+import {Link} from "react-router-dom";
 
 const HomeScreen = () => {
     return (
         <div>
         <NavBar />
+        <br/>
         <Container>
         <img src={Loggo} alt="Logo" className="img-fluid"/> 
         </Container>
+        <Link className="nav-link" to="/signin" >Sign In</Link>             
+        <Link className="nav-link" to="/signup" >Sign Up</Link>
         </div>
         
     );
